2e29dcc09a2eda9b067cbdabdb092f461d9f5cc9
Michaël Bourjac <michael.bourjac@gmail.com>
2024-07-17 08:13:24 +0200

    fix: center home content vertically

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 export const HomePage = () => {
   return (
-    <div className="flex w-full items-center">
+    <div className="flex h-full w-full items-center">
       <div className="mx-auto flex h-[min(calc((100vw-2rem)/2),calc(100vh-2rem))] w-full max-w-[calc(100vh-2rem)] items-center justify-center rounded-full bg-off-black">
         <h1 className="text-4xl uppercase italic text-primary">
           UI components
