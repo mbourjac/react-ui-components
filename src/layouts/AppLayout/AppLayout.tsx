c03b56cfeb84dd7bc1205fd97db7ddabdb954515
Michaël Bourjac <michael.bourjac@gmail.com>
2024-07-25 08:38:23 +0200

    feat: style hovered and active nav links

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from '@tanstack/react-router';
 import { COMPONENTS_DATA } from '../../App.constants';
+import { cn } from '../../lib/tailwind';
 
 export const AppLayout = () => {
   return (
@@ -10,30 +11,47 @@ export const AppLayout = () => {
         </div>
       </main>
       <div className="p-4 pl-0">
-        <nav className="flex h-[calc(100vh-2rem)] flex-col gap-4 overflow-auto rounded-2xl border border-off-black bg-primary p-4">
-          {COMPONENTS_DATA.map(({ pathname, name, tags }) => (
-            <Link
-              key={name}
-              to={pathname}
-              className="group flex flex-col border-b border-off-black text-left transition-all"
-            >
-              <div className="flex flex-col gap-1">
+        <nav>
+          <ul className="flex h-[calc(100vh-2rem)] flex-col gap-4 overflow-auto rounded-2xl border border-off-black bg-primary p-4">
+            {COMPONENTS_DATA.map(({ pathname, name, tags }) => (
+              <li
+                key={name}
+                className="flex flex-col gap-1 border-b border-off-black text-left transition-all"
+              >
                 {tags && (
                   <ul className="flex gap-4">
                     {tags.map((tag) => (
                       <li
                         key={tag}
-                        className="rounded-full border border-off-black px-3 py-1 text-sm"
+                        className="flex h-7 items-center rounded-full border border-off-black px-3 text-sm"
                       >
                         {tag}
                       </li>
                     ))}
                   </ul>
                 )}
-                <h2 className="text-lg uppercase">{name}</h2>
-              </div>
-            </Link>
-          ))}
+                <Link
+                  to={pathname}
+                  className="group flex items-center justify-between text-lg uppercase"
+                >
+                  {({ isActive }) => {
+                    return (
+                      <>
+                        <span>{name}</span>
+                        <span
+                          aria-hidden="true"
+                          className={cn(
+                            'hidden size-4 rounded-full border border-off-black group-hover:inline-block',
+                            isActive && 'inline-block bg-off-black',
+                          )}
+                        ></span>
+                      </>
+                    );
+                  }}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </nav>
       </div>
     </div>
